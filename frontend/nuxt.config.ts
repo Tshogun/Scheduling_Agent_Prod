7d2f9fa92c373546273299c94bb27976eb5b0104
Tshogun <n.b24fnmail@gmail.com>
2025-08-20 22:44:21 +0530

    linting added

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,12 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/test-utils'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/test-utils",
+  ],
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
