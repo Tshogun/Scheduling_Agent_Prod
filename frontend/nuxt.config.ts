24a012546a26eca603f5549c1cf884f0d1bca3fd
Tshogun <n.b24fnmail@gmail.com>
2025-08-21 10:10:50 +0530

    basic frontend ui added

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,15 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
+  vite: {
+    plugins: [
+      tailwindcss(),
+      tsconfigPaths(),
+    ],
+  },
+  css: [resolve(__dirname, "assets/css/main.css")],
   modules: [
     "@nuxt/eslint",
     "@nuxt/image",
     "@nuxt/ui",
     "@nuxt/test-utils",
   ],
-
   eslint: {
     config: {
       standalone: false,
