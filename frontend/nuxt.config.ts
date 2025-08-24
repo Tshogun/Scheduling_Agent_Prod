8bdafa31cf027b57509fb1c4e62a36361e16ed31
Tshogun <n.b24fnmail@gmail.com>
2025-08-24 14:50:27 +0530

    a;flad

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,6 +2,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// eslint-disable-next-line node/no-process-env
+const supabaseUrl = process.env.SUPABASE_URL;
+// eslint-disable-next-line node/no-process-env
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
@@ -22,17 +27,13 @@ export default defineNuxtConfig({
     public: {
       // eslint-disable-next-line node/no-process-env
       apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8080",
-      // eslint-disable-next-line node/no-process-env
-      supabaseUrl: process.env.SUPABASE_URL,
-      // eslint-disable-next-line node/no-process-env
-      supabaseKey: process.env.SUPABASE_KEY,
+      supabaseUrl,
+      supabaseKey,
     },
   },
   supabase: {
-    // eslint-disable-next-line node/no-process-env
-    url: process.env.SUPABASE_URL,
-    // eslint-disable-next-line node/no-process-env
-    key: process.env.SUPABASE_KEY,
+    url: supabaseUrl,
+    key: supabaseKey,
     redirectOptions: {
       login: "/login", // Where to redirect guest users
       callback: "/index", // Where to redirect authenticated users
